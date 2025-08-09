1b0ba822788a1e8109f5867639dcfdc5fff5bea1
gauravnetes <gouravchandra935@gmail.com>
2025-08-09 12:44:08 +0530

    deployment changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 // // to extract json data out of body
 // // run this beofore calling any routes as the routes will be needing
@@ -34,14 +34,14 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-//   });
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
-  // connectDB();
+  connectDB();
 });
