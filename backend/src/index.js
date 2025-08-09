a5a59cc4772d91e59cf7b7e948baf676f6e0cf28
gauravnetes <gouravchandra935@gmail.com>
2025-08-09 13:31:04 +0530

    configure CORS for production

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,9 +22,11 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
+
+const allowedOrigins = [process.env.FRONTEND_URL, "http://localhost:5173"];
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: allowedOrigins,
     credentials: true,
   })
 );
