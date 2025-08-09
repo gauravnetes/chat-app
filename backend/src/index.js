98cdd8a91a3d950d41d2138f70a8094d6461b705
gauravnetes <gouravchandra935@gmail.com>
2025-08-09 12:32:59 +0530

    deployment changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,12 +24,12 @@ const PORT = process.env.PORT;
 // // parsing json data which won't work if run before express.json() middleware
 app.use(express.json({ limit: "30mb" }));
 app.use(cookieParser());
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 // app.use("/api/auth", authRoutes);
 // app.use("/api/messages", messageRoutes);
