b585396c4a6637845929779af99a8b02f55705ad
gauravnetes <gouravchandra935@gmail.com>
2025-08-09 12:22:01 +0530

    deployment changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,31 +17,31 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
-// to extract json data out of body
-// run this beofore calling any routes as the routes will be needing
-// parsing json data which won't work if run before express.json() middleware
-app.use(express.json({ limit: "30mb" }));
-app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+// // to extract json data out of body
+// // run this beofore calling any routes as the routes will be needing
+// // parsing json data which won't work if run before express.json() middleware
+// app.use(express.json({ limit: "30mb" }));
+// app.use(cookieParser());
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
 
 // app.use("/api/auth", authRoutes);
 // app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
 
 server.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
-  connectDB();
+  // connectDB();
 });
