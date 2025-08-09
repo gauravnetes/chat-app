c95a331f1e517e2f70aad77409c865eb0dfd8cd8
gauravnetes <gouravchandra935@gmail.com>
2025-08-09 12:50:35 +0530

    deployment changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,26 +3,24 @@ console.log(JSON.stringify(process.env, null, 2));
 console.log("------------------------------------");
 
 import express from "express";
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
-import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { app, server } from "./lib/socket.js";
 
 import path from "path";
+
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
-
 const __dirname = path.resolve();
 
-// // to extract json data out of body
-// // run this beofore calling any routes as the routes will be needing
-// // parsing json data which won't work if run before express.json() middleware
-app.use(express.json({ limit: "30mb" }));
+app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
@@ -36,12 +34,13 @@ app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
 server.listen(PORT, () => {
-  console.log(`Server is running on PORT: ${PORT}`);
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
